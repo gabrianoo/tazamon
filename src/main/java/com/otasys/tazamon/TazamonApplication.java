725b8bfe9da4bb61b57c2163dbecfe4d59efba99
Ahmed Hassanien <ahmed.hassanien@oracle.com>
2016-07-11 09:22:52 +0200

    iCloud username and password are passed through java program arguments.

--- src/main/java/com/otasys/tazamon/TazamonApplication.java
+++ src/main/java/com/otasys/tazamon/TazamonApplication.java
@@ -1,6 +1,7 @@
 package com.otasys.tazamon;
 
 import com.otasys.tazamon.web.dav.WebDavService;
+import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -8,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.inject.Inject;
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class TazamonApplication implements CommandLineRunner {
@@ -22,7 +24,11 @@ public class TazamonApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
+        String auth = strings[0] + ":" + strings[1];
+        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")) );
+        String authHeader = "Basic " + new String( encodedAuth );
         LOGGER.info("Server support WEB DAV {}", webDavService.isWebDavSupported());
+        webDavService.getUserPrincipalUnifiedId(authHeader);
     }
 
 }
