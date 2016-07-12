eef0098816165474d1ef6f493205a15943cab51a
Ahmed Hassanien <ahmed.hassanien@oracle.com>
2016-07-12 13:46:47 +0200

    Added debug logging to tests to enhance coverage. Renamed main application test class and marked it as integration test.

--- src/main/java/com/otasys/tazamon/TazamonApplication.java
+++ src/main/java/com/otasys/tazamon/TazamonApplication.java
@@ -1,7 +1,6 @@
 package com.otasys.tazamon;
 
 import com.otasys.tazamon.web.dav.WebDavService;
-import org.apache.commons.codec.binary.Base64;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -9,7 +8,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import javax.inject.Inject;
-import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class TazamonApplication implements CommandLineRunner {
@@ -18,17 +16,13 @@ public class TazamonApplication implements CommandLineRunner {
     private WebDavService webDavService;
     private static final Logger LOGGER = LoggerFactory.getLogger(TazamonApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(TazamonApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TazamonApplication.class, args);
+    }
 
-	@Override
-	public void run(String... strings) throws Exception {
-        String auth = strings[0] + ":" + strings[1];
-        byte[] encodedAuth = Base64.encodeBase64(auth.getBytes(Charset.forName("US-ASCII")) );
-        String authHeader = "Basic " + new String( encodedAuth );
+    @Override
+    public void run(String... strings) throws Exception {
         LOGGER.info("Server support WEB DAV {}", webDavService.isWebDavSupported());
-        webDavService.getUserPrincipalUnifiedId(authHeader);
     }
 
 }
--- src/test/java/com/otasys/tazamon/TazamonApplicationIntegrationTest.java
+++ src/test/java/com/otasys/tazamon/TazamonApplicationIntegrationTest.java
@@ -1,15 +1,13 @@
 package com.otasys.tazamon;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(TazamonApplication.class)
-public class TazamonApplicationTests {
+public class TazamonApplicationIntegrationTest {
 
     @Test
     public void givenSpringBootApplicationWhenApplicationStartsThenSpringContextShouldLoadSuccessfully() {
